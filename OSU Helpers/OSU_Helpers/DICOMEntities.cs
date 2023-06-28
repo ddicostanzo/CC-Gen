71f21f3041e966bf511d055eab47dd40bce8a97f
Dominic DiCostanzo <dicostanzo.2@osu.edu>
2023-06-28 11:11:03 -0400

    Update DICOM Entities

--- OSU Helpers/OSU_Helpers/DICOMEntities.cs
+++ OSU Helpers/OSU_Helpers/DICOMEntities.cs
@@ -24,64 +24,6 @@ namespace OSU_Helpers
         /// </summary>
         public string IP { get; set; }
 
-        /// <summary>
-        /// Dev_Physics DICOM entity
-        /// </summary>
-        /// <returns>DICOMEntityConnection with Dev_Physics settings</returns>
-        public static DICOMEntityConnections Dev_Physics()
-        {
-            DICOMEntityConnections de = new DICOMEntityConnections()
-            {
-                Port = 54000,
-                IP = "10.65.160.105",
-                AE_Title = "DCM_PHYSICS"
-            };
-            return de;
-        }
-
-        /// <summary>
-        /// Dev_Script DICOM entity
-        /// </summary>
-        /// <returns>DICOMEntityConnection with Dev_Script settings</returns>
-        public static DICOMEntityConnections Dev_Script()
-        {
-            DICOMEntityConnections de = new DICOMEntityConnections()
-            {
-                Port = 54000,
-                IP = "10.216.14.135",
-                AE_Title = "DCM_SCRIPT"
-            };
-            return de;
-        }
-
-        /// <summary>
-        /// Production DICOM entity
-        /// </summary>
-        /// <returns>DICOMEntityConnection with Production settings</returns>
-        public static DICOMEntityConnections Production()
-        {
-            DICOMEntityConnections de = new DICOMEntityConnections()
-            {
-                Port = 54000,
-                IP = "10.95.25.9",
-                AE_Title = "VMSDB_OSU"
-            };
-            return de;
-        }
-
-        /// <summary>
-        /// Dev_Physics_FS DICOM entity
-        /// </summary>
-        /// <returns>DICOMEntityConnection with Dev_Physics File Service settings</returns>
-        public static DICOMEntityConnections Dev_Physics_FS()
-        {
-            DICOMEntityConnections de = new DICOMEntityConnections()
-            {
-                Port = 104,
-                IP = "10.65.160.105",
-                AE_Title = "VMSFSD"
-            };
-            return de;
-        }
+      
     }
 }
